d85958daa759fede3a8d3d7c6fa0768a6951c194
timotismjntk <timotiussimanjuntak24@gmail.com>
2025-11-12 09:49:18 +0700

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
